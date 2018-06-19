41e9c0913fcc3fbf097b54d12ce5ad07eb05e5b0
Purnendu Rath (puru_cr7) <Purnendu Rath (puru_cr7)@DESKTOP-SMOVB8O>
2018-06-19 17:38:14 +0530

    First commit for MathUtils class, will update more methods subsequently.

---
+++ CodingContests/src/com/puru_cr7/mylib/MathUtils.java
@@ -0,0 +1,15 @@
+package com.puru_cr7.mylib;
+
+/**
+ * This class is a library for all math related methods with best possible complexities.
+ *
+ * @author Purnendu Rath (puru_cr7).
+ *         Created Jun 19, 2018.
+ */
+public class MathUtils {
+
+	public static boolean isOdd(final int n) {
+		return (n & 1) != 0;
+	}
+
+}
