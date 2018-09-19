1c1f433ad6426899e9958bae39132241266408dc
Purnendu Rath (puru_cr7) <Purnendu Rath (puru_cr7)@puru_cr7-PC>
2018-09-19 23:26:23 +0530

    added two new methods

--- CodingContests/src/com/puru_cr7/mylib/MathUtils.java
+++ CodingContests/src/com/puru_cr7/mylib/MathUtils.java
@@ -12,4 +12,25 @@ public class MathUtils {
 		return (n & 1) != 0;
 	}
 
+	public static boolean isPrime(final long no) {
+		for (long i = 2; i * i <= no; i++) {
+			if (no % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static long[][] generatePascalsTriangle(final int n, final int mod) {
+		final long[][] ps = new long[n + 1][];
+		for (int i = 1; i <= n; i++) {
+			ps[i] = new long[i + 1];
+			ps[i][0] = 1l;
+			ps[i][i] = 1l;
+			for (int j = 1; j < i; j++) {
+				ps[i][j] = ps[i - 1][j - 1] + ps[i - 1][j];
+			}
+		}
+		return ps;
+	}
 }
