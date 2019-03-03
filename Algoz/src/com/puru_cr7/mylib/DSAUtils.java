fed72aa033bfb7db37529a7fbeecdcb8c7eb30b7
Purnendu Rath (puru_cr7) <Purnendu Rath (puru_cr7)@DESKTOP-8JOSU2U>
2019-03-03 17:00:47 +0530

    update

--- Algoz/src/com/puru_cr7/mylib/DSAUtils.java
+++ Algoz/src/com/puru_cr7/mylib/DSAUtils.java
@@ -230,4 +230,30 @@ public class DSAUtils {
 		return result;
 	}
 
+	public static int daysInMonth(final int mon) {
+		switch (mon) {
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				return 31;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				return 30;
+			case 2:
+				return 28;
+		}
+		return 0;
+	}
+
+	public static boolean isLeapYear(final int year) {
+		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+	}
+
 }
